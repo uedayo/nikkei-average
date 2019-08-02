860792be0804a158699abe123fcee74874636043
uedayo <uedayo@gmail.com>
2019-08-02 12:07:00 +0900

    get Nikkei Stock Average

--- getNikkeiAverage.py
+++ getNikkeiAverage.py
@@ -3,9 +3,27 @@ import urllib2
 from bs4 import BeautifulSoup
 
 STOCK_URL = "http://www.nikkei.com/markets/kabu/"
+TARGET_CLASS = "mkc-stock_prices"
 
 html = urllib2.urlopen(STOCK_URL)
 
 soup = BeautifulSoup(html, "html.parser")
 
-print soup.title.string
+# pick all span elements
+span = soup.find_all("span")
+
+# initialize to avoid print error
+nikkei_average = ""
+
+# find span element which has TARGET_CLASS out from every span elements
+for tag in span:
+    try:
+        string_ = tag.get("class").pop(0)
+
+        if string_ in TARGET_CLASS:
+            nikkei_average = tag.string
+            break
+    except:
+        pass
+
+print nikkei_average
